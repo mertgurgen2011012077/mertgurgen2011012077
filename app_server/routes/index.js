a0028165803da05b442b125e592feb5d6fcfefde
mertgurgen <l2011012077@stud.sdu.edu.tr>
2020-11-18 22:27:24 +0300

    ilk gonderim

---
+++ app_server/routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var ctrlMain = require('C:\Users\Mert Gürgen\Desktop\mekan32\app_server\controllers\main');
+
+/* GET home page. */
+router.get('/', ctrlMain.index) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
